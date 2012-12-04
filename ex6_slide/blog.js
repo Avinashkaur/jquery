8967f915e375c7d774667c4ec3dc0691099e4fb5
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-04 17:24:05 +0530

    revisedslider

--- ex6_slide/blog.js
+++ ex6_slide/blog.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
-  var blog_headlines = $('#blog ul li');
-  blog_headlines.each(function(){
+  $('#blog ul li').each(function(){
     var list_item = $(this);
-
-    list_item.find('h3').find('a').each(function() {
-      // var heading = $(this);
-      $(this).click(function(link_object) {
-        link_object.preventDefault();
-        // blog_headlines.find('p').each(function() {
-        //   $(this).slideUp("fast");
-        // });
-        $(this).
-
-        list_item.find('p').slideDown();
+    list_item.find('h3').find('a').click(function(eventObject){
+      eventObject.preventDefault();
+      list_item.siblings().find('p').each(function() {
+        $(this).slideUp("fast");
       });
+      list_item.find('p').slideDown();
     });
   });
 });
