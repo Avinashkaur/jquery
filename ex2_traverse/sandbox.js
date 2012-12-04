d1cfd44aba535d7c90146ffb2d2343bbda7203b5
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-04 13:47:46 +0530

    revised

--- ex2_traverse/sandbox.js
+++ ex2_traverse/sandbox.js
@@ -1,25 +1,18 @@
 $(document).ready( function() {
-
   //log alt attribute of all img tags
   $('img').each(function(i) {
     console.log(this.alt);
   });
-
   //search input text box and traverse up to the form and class to the form
   console.log($(".input_text").parent().attr('class' , 'searchbox'));
-
   //Select the list item inside #myList that has a class of "current" 
   // and remove that class from it; add a class of "current" to the next list item.
-  console.log($("#myList .current").next('li').addClass('current'));
-  console.log($("#myList .current").removeClass('current'));
-
-  //Select the select element inside #specials; traverse your way to the submit button. 
-  var myform = $('#specials').children('form');
-  console.log($(myform).children('ul').children('li').children('input'));
-
+  console.log($("#myList .current").removeClass('current').next('li').addClass('current'));
+  //Select the select element inside #specials; traverse your way to the submit button.
+  console.log($('#specials').find('#goButton')); 
   // Select the first list item in the #slideshow element; add the class "current" to it,
   //  and then add a class of "disabled" to its sibling elements. 
-  console.log($('#slideshow li:first').addClass('.current'));
-  console.log($('#slideshow li:first').siblings().addClass('.disabled'));
-  }
-  );
+  console.log($('#slideshow li:first').addClass('.current').siblings().each(function(){
+    $(this).addClass('.disabled');
+  }));
+});
