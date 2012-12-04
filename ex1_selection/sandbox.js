32c2f969acbdfee26e6c4955bf8ed7a599b11c54
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-04 13:19:25 +0530

    revised

--- ex1_selection/sandbox.js
+++ ex1_selection/sandbox.js
@@ -1,6 +1,12 @@
 $(document).ready(function() { 
  $("div.module").css("background-color" , "red"); 
+ 
+ //three ways to reach 3rd list item
  $("#myList li").eq(2).css("background-color" , "cyan");
+ $("#main").find("#myList #myListItem").css("background-color" , "cyan");
+ $("#myListItem").css("background-color" , "cyan");
+//best way is the third one as it refers directly by the id of the element and there is no depenedency on DOM
+
  $("label[for = 'q']").css("background-color" , "yellow");
  console.log("hidden elements:" + $("*:hidden").length);
  console.log("img with alt attribute:" + $('img[alt]').length);
