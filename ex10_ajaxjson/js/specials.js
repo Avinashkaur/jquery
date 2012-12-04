b7e4a696fa288f898e4b14979b122955c7305736
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-04 18:09:40 +0530

    revised10

--- ex10_ajaxjson/js/specials.js
+++ ex10_ajaxjson/js/specials.js
@@ -8,12 +8,16 @@ $(document).ready(function(){
         var $img = $("<img />");
         $.getJSON('data/specials.json' , function(data) {
             cached_JSON = data;
-            // $img.attr("src" , cached_JSON[$value].image);
-            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text + "<br />" + cached_JSON[$value].color + "<br />" + $img.setAttribute("src" , cached_JSON[$value].image));
+            
+            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text + "<br />" + cached_JSON[$value].color);
+            $img.attr("src" , cached_JSON[$value].image);
+            $img.insertAfter($div);
         });
         $(this).change(function(){
             $value = $(this).val();
-            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text + "<br />" + cached_JSON[$value].color + "<br />" + $img) ;
+            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text);
+            $img.attr("src" , cached_JSON[$value].image);
+            $img.insertAfter($div);
         });
     });
 });
