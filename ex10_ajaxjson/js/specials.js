bdb9838e4795854082e53be379c83a34b5587649
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-04 17:38:53 +0530

    revisedtraverse

--- ex10_ajaxjson/js/specials.js
+++ ex10_ajaxjson/js/specials.js
@@ -2,16 +2,18 @@ $(document).ready(function(){
     var $div = $("<div></div>").insertAfter($("#specials").find('form'));
     var $days = $("#specials").find('form ul li select');
     var cached_JSON = {};
-    $(".input_submit").remove();
     $days.one('change' , function(){
         var $value = $(this).val();
+        $(".input_submit").remove();
+        var $img = $("<img />");
         $.getJSON('data/specials.json' , function(data) {
             cached_JSON = data;
-            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text);
+            // $img.attr("src" , cached_JSON[$value].image);
+            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text + "<br />" + cached_JSON[$value].color + "<br />" + $img.setAttribute("src" , cached_JSON[$value].image));
         });
         $(this).change(function(){
             $value = $(this).val();
-            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text) ;
+            $div.html(cached_JSON[$value].title + "<br />" + cached_JSON[$value].text + "<br />" + cached_JSON[$value].color + "<br />" + $img) ;
         });
     });
 });
