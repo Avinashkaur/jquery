47cf8b08b61f77c1f14d9d899cac1a8a7c49329b
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-04 16:35:39 +0530

    revised_1

--- ex4_inputhint/inputHint.js
+++ ex4_inputhint/inputHint.js
@@ -1,18 +1,19 @@
 $(document).ready( function() {
   var inputText = $(".input_text");
   var labelText = $("label[for = 'q']");
+  var oldLabelText = labelText;
   inputText.attr('value' , labelText.html());
   inputText.addClass("hint");
   labelText.detach();
   console.log(inputText.focus(function() {
-    if (inputText.attr("value") == "Enter search term") {
+    if (inputText.attr("value") == oldLabelText.html()) {
       inputText.attr("value" , "");
        inputText.removeClass("hint");
     }
     console.log(inputText.blur(function() {
       var text_value = inputText.attr("value").trim();
         if(text_value == "") {
-          inputText.attr("value" , "Enter search term");
+          inputText.attr("value" , oldLabelText.html());
           inputText.addClass("hint");
         }
     }));
