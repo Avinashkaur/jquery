b201302066222e110b5ac4a0f93b89b1b94d1caa
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-05 10:41:23 +0530

    fixed

--- ex4_inputhint/inputHint.js
+++ ex4_inputhint/inputHint.js
@@ -3,21 +3,17 @@ $(document).ready( function() {
   $(".input_text").val(labelText);
   $(".input_text").addClass("hint");
   $("label[for = 'q']").detach();
-  
- 
     
     $(".input_text").focus(function() {
-      if ($(this).val() == labelText) {
+      if ($(this).hasClass("hint")) {
         $(this).attr("value" , "");
         $(this).removeClass("hint");
       }
     });
-
     $(".input_text").blur(function() {
       var text_value = $(this).val().trim();
       if(text_value == "") {
         $(this).val(labelText).addClass("hint");
       }
     });
-  
 });
