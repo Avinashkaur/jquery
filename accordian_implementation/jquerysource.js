3767e279abf6a4c9a56bafd01d4148ab97196b21
Avinash kaur <avinash.kaur@vinsol.com>
2012-12-11 16:30:11 +0530

    revised

--- accordian_implementation/jquerysource.js
+++ accordian_implementation/jquerysource.js
@@ -1,9 +1,18 @@
-$(document).ready(function() {
+//assigns unique ids to leaf node
+function uniqueIDsToLeafNodes() {
   var $listItem = $('li:not(:has(ul))');
   var number = 0;
-  $listItem.each(function(index){
+  $listItem.each(function(){
     $(this).attr("id",number);
-    $(this).find("a").attr("href","allornone.html?"+number);
-    number++;
+      $(this).find("a").attr("href","allornone.html?"+number);
+      number++;
+    });
+  } 
+function check_url() {
+  var url = $(location).attr('href');
+  var node_id = url.split("?");
+  var leafnode_parents = $('li #' + node_id[1]).parents('.accordion');
+  leafnode_parents.each(function() {
+    $(this).css("display" , "block")
   });
-});
+}
