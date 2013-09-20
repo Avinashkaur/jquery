f541fc260443aca702ba5c95d826de3cd9533c27
Avinashkaur <avinash.kaur@vinsol.com>
2013-09-20 12:30:16 +0530

    multiple selection option added for a single filter

--- product_brand_latest/js/filter_products.js
+++ product_brand_latest/js/filter_products.js
@@ -1,6 +1,4 @@
 var items = [];
-var productArray = {};
-
 var loadJSON = function() {
   $.getJSON('product_data/product.json', function(data) {
   	$.each(data,function(key,val) {
@@ -19,36 +17,57 @@ var loadJSON = function() {
 
 var showAllProducts  = function(products) {
 	$.each(products, function(key, val) {
-		var newLi = $("<li />").attr("data-color", val.color).attr("data-brand", val.brand).attr("sold-out", val.sold_out).attr("data-latest", val.latest);
+		var newLi = $("<li />").attr("data-brand", val.brand).attr("data-color", val.color).attr("data-sold", val.sold_out);
 		$("<img />").attr("src", val.url).appendTo(newLi);
 		newLi.appendTo("#products_display");
 	});
 }
 
 var showSelectedItems = function(element) {
-	var checkedId = element.attr("value"), attribute = "";
-	var parentId = element.parent(".list_items").attr("id");
-	if (element.is(":checked")) {
-		productArray["data-" + parentId] = "'" + checkedId + "'"; 
+	var options = [], elements = [], possibles = [];
+
+	jQuery('.list_items').each(function() {
+    opts = [];
+    jQuery('input:checked', this).each(function() {
+        parentId = $(this).parent('.list_items').attr('id');
+        opts.push("[data-" + parentId + "='" + jQuery(this).attr('value') + "']");
+        elements.push(opts);
+    });
+    options.push(opts);
+	});
+	newArr = jQuery.map(elements, function(n) { return n });
+	
+	recursiveSearch = function(text, depth) {
+		text = text || "";
+		depth = depth || 0;
+
+		for (var i = 0; i < options[depth].length; i++) {
+      if(depth+1 < options.length)
+      	recursiveSearch(text + ((text == "")?"":"") + options[depth][i],depth + 1);
+      else 
+      	possibles.push(text + options[depth][i]);
+		}
 	}
-	else {
-		delete productArray["data-" + parentId];
-	}
-	$.each(productArray, function(key, value) {
-		attribute += "[" + key + "=" + value + "]";
-	})	
-	$("#products_display li").hide();
-	$("#products_display li" + attribute).show();
+	recursiveSearch();
+	$('#products_display li').hide();
+	showProducts(newArr, 0);	
+	showProducts(possibles, 1);
+}
+
+var showProducts = function(array, hideAll) {
+	console.log(array);
+	var len = array.length;
+	if (hideAll && len) { $('#products_display li').hide(); }
+	$.each(array, function(index, val) {
+		$('#products_display li' + val).show();
+	});
 }
 
 $(".list_items").delegate("input[type='checkbox']", "click", function() {
 	showSelectedItems($(this));
-	$(this).siblings("input[type=checkbox]").each(function() {
-    $(this).attr("checked",false);
-  });
 });
 
 $("#available_products").click(function() {
-	$("#products_display li[sold-out=1]").hide();
+	$("li[data-sold=1]").hide();
 })
 loadJSON();
